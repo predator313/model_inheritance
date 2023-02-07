1c8e0f723ef8193ecca48f6929044dbbd8ed1cfc
Aamir Ashraf <aamirnasa97@gmail.com>
2023-02-07 18:36:26 +0530

    retriving data is working successfully

--- app/views.py
+++ app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Student,Teacher,Contractor
 
 # Create your views here.
+def fun(request):
+    student_data=Student.objects.all()
+    teacher_data=Teacher.objects.all()
+    contractor_data=Contractor.objects.all()
+    return render(request,'app/home.html',{'student_data':student_data,'teacher_data':teacher_data,'contractor_data':contractor_data})
