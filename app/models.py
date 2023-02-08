5613ba81226aa6ea97d0c8d253ff9cde51b180c2
Aamir Ashraf <aamirnasa97@gmail.com>
2023-02-08 13:01:49 +0530

    all model inheritance is working fine

--- app/models.py
+++ app/models.py
@@ -44,7 +44,7 @@ class Bacche(Exam_Center):
 
 #proxy models inheritance concept
 class A(models.Model):
-    name=models.CharField()
+    name=models.CharField(max_length=20)
     roll=models.IntegerField()
 class B(A):
     class Meta:
